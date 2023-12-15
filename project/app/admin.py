cce6430e9bfb213a83addff1c400d1361b5019f7
Joy097 <shihabrumeechowdhury@gmail.com>
2023-12-15 14:35:14 +0600

    update 6

--- project/app/admin.py
+++ project/app/admin.py
@@ -4,3 +4,7 @@ from django.contrib import admin
 from .models import Variant, Product,ProductImage,ProductVariant,ProductVariantPrice
 
 admin.site.register(Variant)
+admin.site.register(Product)
+admin.site.register(ProductImage)
+admin.site.register(ProductVariant)
+admin.site.register(ProductVariantPrice)
