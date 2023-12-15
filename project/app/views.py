05a46a7c3253cddd54adce5a11844d98c88e60ca
Joy097 <shihabrumeechowdhury@gmail.com>
2023-12-15 12:42:37 +0600

    update 0

--- project/app/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def say_hello(request): 
-    return HttpResponse('Hello World')
-
-def show_page(request): 
-    return render(request,'home.html')
